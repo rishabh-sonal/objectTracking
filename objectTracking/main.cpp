73b2ff5897473bbc2f5fc6ecadb22a94bcb2b2a0
rishabh-sonal <rishabh@techolution.com>
2017-03-30 19:27:24 +0530

    Initial Commit

---
+++ objectTracking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  objectTracking
+//
+//  Created by Rishabh on 30/03/17.
+//  Copyright © 2017 Rishabh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
